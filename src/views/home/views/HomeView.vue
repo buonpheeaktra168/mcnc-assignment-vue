adba3f254184a57c73ca65ebb2a0477fd3063899
tra <buon_pheaktra@bb.bbu.edu.kh>
2022-12-27 16:13:24 +0700

    prepare project structure

---
+++ src/views/home/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <a href="/notification"><button>Notification</button></a>
+  <a href="/fetchUser"><button>Fetch User From store</button></a>
+  <a href="/postRequest"><button>Post Request</button></a>
+</template>
+
+<script>
+import { ref, reactive } from 'vue'
+
+export default {
+  
+
+}
+</script>
+<style>
+
+</style>
